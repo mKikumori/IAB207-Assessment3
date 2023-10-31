8201885624f2df2a29207399e11b0b533da7778b
Matheus Kikumori Almeida Leite <matheuskiku@gmail.com>
2023-10-31 20:47:31 +1000

    modified:   a3_starter_code-main/projectfile/website/__pycache__/forms.cpython-311.pyc 	modified:   a3_starter_code-main/projectfile/website/__pycache__/views.cpython-311.pyc 	modified:   a3_starter_code-main/projectfile/website/templates/base.html 	modified:   a3_starter_code-main/projectfile/website/templates/index.html 	modified:   a3_starter_code-main/projectfile/website/views.py

--- a3_starter_code-main/projectfile/website/views.py
+++ a3_starter_code-main/projectfile/website/views.py
@@ -13,9 +13,35 @@ def index():
 @mainbp.route('/search')
 def search():
     if request.args['search'] and request.args['search'] != "":
-        print(request.args['search'])
         query = "%" + request.args['search'] + "%"
         music_shows = db.session.scalars(db.select(MusicShow).where(MusicShow.name.like(query)))
+        return render_template('index.html', music_shows=music_shows)
+    else:
+        return redirect(url_for('main.index'))
+    
+@mainbp.route('/filter_location')
+def filter_location():
+    if request.args['filter_location'] and request.args['filter_location'] != "":
+        query = "%" + request.args['filter_location'] + "%"
+        music_shows = db.session.scalars(db.select(MusicShow).where(MusicShow.location.like(query)))
+        return render_template('index.html', music_shows=music_shows)
+    else:
+        return redirect(url_for('main.index'))
+    
+@mainbp.route('/filter_artists')
+def filter_artists():
+    if request.args['filter_artists'] and request.args['filter_artists'] != "":
+        query = "%" + request.args['filter_artists'] + "%"
+        music_shows = db.session.scalars(db.select(MusicShow).where(MusicShow.artists.like(query)))
+        return render_template('index.html', music_shows=music_shows)
+    else:
+        return redirect(url_for('main.index'))
+    
+@mainbp.route('/filter_genre')
+def filter_genre():
+    if request.args['filter_genre'] and request.args['filter_genre'] != "":
+        query = "%" + request.args['filter_genre'] + "%"
+        music_shows = db.session.scalars(db.select(MusicShow).where(MusicShow.genre.like(query)))
         return render_template('index.html', music_shows=music_shows)
     else:
         return redirect(url_for('main.index'))
