560cecd73fe11edf7fadcb03f96edfcea5020b9f
Matheus Kikumori Almeida Leite <matheuskiku@gmail.com>
2023-11-02 15:00:18 +1000

    modified:   a3_starter_code-main/projectfile/instance/mydbname.sqlite 	modified:   a3_starter_code-main/projectfile/website/__pycache__/forms.cpython-311.pyc 	modified:   a3_starter_code-main/projectfile/website/__pycache__/music_shows.cpython-311.pyc 	modified:   a3_starter_code-main/projectfile/website/forms.py 	modified:   a3_starter_code-main/projectfile/website/music_shows.py 	new file:   a3_starter_code-main/projectfile/website/static/image/John.jpeg 	deleted:    a3_starter_code-main/projectfile/website/static/image/Rome-Italy.jpg 	deleted:    a3_starter_code-main/projectfile/website/static/image/The Bluebottles and katanak.png 	deleted:    a3_starter_code-main/projectfile/website/static/image/The_Netherlands.jpg 	new file:   a3_starter_code-main/projectfile/website/static/image/akk.jpg 	new file:   a3_starter_code-main/projectfile/website/static/image/akk2.jpg 	new file:   a3_starter_code-main/projectfile/website/static/image/def.jpg 	modified:   a3_starter_code-main/projectfile/website/templates/index.html

--- a3_starter_code-main/projectfile/website/forms.py
+++ a3_starter_code-main/projectfile/website/forms.py
@@ -6,40 +6,56 @@ from flask_wtf.file import FileRequired, FileField, FileAllowed
 
 ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
-#creates the login information
+# creates the login information
+
+
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    user_name = StringField("User Name", validators=[
+                            InputRequired('Enter user name')])
+    password = PasswordField("Password", validators=[
+                             InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
  # this is the registration form
+
+
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
+    user_name = StringField("User Name", validators=[InputRequired()])
+    email_id = StringField("Email Address", validators=[
+                           Email("Please enter a valid email")])
+    # linking two fields - password should be equal to data entered in confirm
+    password = PasswordField("Password", validators=[InputRequired(),
+                                                     EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
 
-    #submit button
+    # submit button
     submit = SubmitField("Register")
 
-#User comment
+# User comment
+
+
 class CommentForm(FlaskForm):
     text = TextAreaField('Comment', [InputRequired()])
     submit = SubmitField('Create')
 
 # Create Music Show Form
+
+
 class MusicShowForm(FlaskForm):
     name = StringField("Enter show name...", validators=[InputRequired()])
-    description = TextAreaField("Enter show's description", validators = [InputRequired()])
-    image = FileField('Show Image', validators=[FileRequired(message='Image cannot be empty'), FileAllowed(ALLOWED_FILE, message='Only supports pnj, jpg, JPG, PNG')])
+    description = TextAreaField(
+        "Enter show's description", validators=[InputRequired()])
+    image = FileField('Show Image', validators=[FileRequired(message='Image cannot be empty'), FileAllowed(
+        ALLOWED_FILE, message='Only supports pnj, jpg, JPG, PNG')])
     genre = StringField('Genre', validators=[InputRequired()])
     status = StringField('Status', validators=[InputRequired()])
-    start_date = DateTimeField('Show Start Date', validators=[InputRequired()])
-    end_date = DateTimeField('Show End Date', validators=[InputRequired()])
+    start_date = DateTimeField(
+        'Show Start Date', format="%d/%m/%Y %H:%M %p", validators=[InputRequired()])
+    end_date = DateTimeField(
+        'Show End Date', format="%d/%m/%Y %H:%M %p", validators=[InputRequired()])
     artists = StringField('Show Artists', validators=[InputRequired()])
     location = StringField('Location', validators=[InputRequired()])
-    num_tickets_avaliable = IntegerField('Number of Tickets Avaliable', validators=[InputRequired()])
+    num_tickets_avaliable = IntegerField(
+        'Number of Tickets Avaliable', validators=[InputRequired()])
     promocode = IntegerField('Promocode', validators=[InputRequired()])
     submit = SubmitField("Create")
