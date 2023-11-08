a064939249cf6ce2f95721b1e867fbdd652e71c4
Matheus Kikumori Almeida Leite <matheuskiku@gmail.com>
2023-11-08 22:56:15 +1000

    modified:   a3_starter_code-main/projectfile/website/auth.py 	modified:   a3_starter_code-main/projectfile/website/forms.py 	modified:   a3_starter_code-main/projectfile/website/views.py

--- a3_starter_code-main/projectfile/website/auth.py
+++ a3_starter_code-main/projectfile/website/auth.py
@@ -1,62 +1,63 @@
 from flask import Blueprint, flash, render_template, request, url_for, redirect
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
-from .forms import LoginForm,RegisterForm
-from flask_login import login_user, login_required,logout_user
+from .forms import LoginForm, RegisterForm
+from flask_login import login_user, login_required, logout_user
 from . import db
 
-#create a blueprint
+# create a blueprint
 authbp = Blueprint('auth', __name__)
 
 
-@authbp.route('/register', methods = ['GET', 'POST'])
+@authbp.route('/register', methods=['GET', 'POST'])
 def register():
     register = RegisterForm()
-    if (register.validate_on_submit()==True):
+    if (register.validate_on_submit() == True):
 
         pwd = register.password.data
         email = register.email_id.data
         uname = register.user_name.data
 
-        user = db.session.scalar(db.select(User).where(User.name==uname))
+        user = db.session.scalar(db.select(User).where(User.name == uname))
 
         if user:
             flash('Username already exists, please try another')
             return redirect(url_for('auth.register'))
-        
+
         pwd_hash = generate_password_hash(pwd)
-        
+
         new_user = User(name=uname, password_hash=pwd_hash, emailid=email)
         db.session.add(new_user)
         db.session.commit()
-        
+
         return redirect(url_for('main.index'))
     else:
         return render_template('user.html', form=register, heading='Register')
-    
+
 
 # this is a hint for a login function
 @authbp.route('/login', methods=['GET', 'POST'])
 def login():
-    
+
     login_form = LoginForm()
-    error=None
-    if(login_form.validate_on_submit()==True):
+    error = None
+    if (login_form.validate_on_submit() == True):
         user_name = login_form.user_name.data
         password = login_form.password.data
-        user = db.session.scalar(db.select(User).where(User.name==user_name))
+        user = db.session.scalar(db.select(User).where(User.name == user_name))
         if user is None:
-            error='Incorrect credentials supplied'
-        elif not check_password_hash(user.password_hash, password): #takes the hash and password
-            error='Incorrect credentials supplied'
+            error = 'Incorrect credentials supplied'
+        # takes the hash and password
+        elif not check_password_hash(user.password_hash, password):
+            error = 'Incorrect credentials supplied'
         if error is None:
             login_user(user)
             return redirect(url_for('main.index'))
         else:
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
-    
-    
+
+
 @authbp.route('/logout')
 @login_required
 def logout():
--- a3_starter_code-main/projectfile/website/views.py
+++ a3_starter_code-main/projectfile/website/views.py
@@ -10,38 +10,46 @@ def index():
     music_shows = db.session.scalars(db.select(MusicShow)).all()
     return render_template('index.html', music_shows=music_shows)
 
+
 @mainbp.route('/search')
 def search():
     if request.args['search'] and request.args['search'] != "":
         query = "%" + request.args['search'] + "%"
-        music_shows = db.session.scalars(db.select(MusicShow).where(MusicShow.name.like(query)))
+        music_shows = db.session.scalars(
+            db.select(MusicShow).where(MusicShow.name.like(query)))
         return render_template('index.html', music_shows=music_shows)
     else:
         return redirect(url_for('main.index'))
-    
+
+
 @mainbp.route('/filter_location')
 def filter_location():
     if request.args['filter_location'] and request.args['filter_location'] != "":
         query = "%" + request.args['filter_location'] + "%"
-        music_shows = db.session.scalars(db.select(MusicShow).where(MusicShow.location.like(query)))
+        music_shows = db.session.scalars(
+            db.select(MusicShow).where(MusicShow.location.like(query)))
         return render_template('index.html', music_shows=music_shows)
     else:
         return redirect(url_for('main.index'))
-    
+
+
 @mainbp.route('/filter_artists')
 def filter_artists():
     if request.args['filter_artists'] and request.args['filter_artists'] != "":
         query = "%" + request.args['filter_artists'] + "%"
-        music_shows = db.session.scalars(db.select(MusicShow).where(MusicShow.artists.like(query)))
+        music_shows = db.session.scalars(
+            db.select(MusicShow).where(MusicShow.artists.like(query)))
         return render_template('index.html', music_shows=music_shows)
     else:
         return redirect(url_for('main.index'))
-    
+
+
 @mainbp.route('/filter_genre')
 def filter_genre():
     if request.args['filter_genre'] and request.args['filter_genre'] != "":
         query = "%" + request.args['filter_genre'] + "%"
-        music_shows = db.session.scalars(db.select(MusicShow).where(MusicShow.genre.like(query)))
+        music_shows = db.session.scalars(
+            db.select(MusicShow).where(MusicShow.genre.like(query)))
         return render_template('index.html', music_shows=music_shows)
     else:
         return redirect(url_for('main.index'))
